61c291fc7c801b734caea29f866e90683aac8d3e
k213088 <127028606+k213088@users.noreply.github.com>
2023-05-07 23:57:04 +0500

    Update main.c

--- Project/main.c
+++ Project/main.c
@@ -1,1 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<stdbool.h>
 
+//  Some global variables
+int i=0, j=0, ind=0, numProcess=0, numResources=0;
+int **alloc, **max, **need, *avail, *safeSequecne;
+
+void main{ // main will just call the function so no return type
+  
+  
+}
