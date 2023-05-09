dcd2d45165231e32612e06c26623cca74e28e160
k213088 <127028606+k213088@users.noreply.github.com>
2023-05-09 23:23:24 +0500

    Update chainsmoker.c

--- Project/chainsmoker.c
+++ Project/chainsmoker.c
@@ -78,8 +78,100 @@ void* agentM()
   }
 }
 
+void* pusherP()
+{
+  while(1)
+  {
+    sem_wait(&pusher_p);
+    sem_wait(&lock);
+    if(isMatches==1)
+    {
+      isMatches = 0;
+      sem_post(&tabacco);
+    }
+    else if(isTobacco==1)
+    {
+      isTobacco = 0;
+      sem_post(&matches);
+    }
+    else
+    {
+      isPaper = 1;
+      sem_post(&lock);
+    }
+  }
+}
+
+void* pusherM()
+{
+  while(1)
+  {
+    sem_wait(&pusher_m);
+    sem_wait(&lock);
+    if(isTobacco==1)
+    {
+      isTobacco = 0;
+      sem_post(&paper);
+    }
+    else if(isPaper==1)
+    {
+      isPaper = 0;
+      sem_post(&tobacco);
+    }
+    else
+    {
+      isMatches = 1;
+      sem_post(&lock);
+    }
+  }
+}
+
+
+void* pusherT()
+{
+  while(1)
+  {
+    sem_wait(&pusher_t);
+    sem_wait(&lock);
+    if(isMatches==1)
+    {
+      isMatches = 0;
+      sem_post(&paper);
+    }
+    else if(isPaper==1)
+    {
+      isPaper = 0;
+      sem_post(&matches);
+    }
+    else
+    {
+      isTobacco = 1;
+      sem_post(&lock);
+    }
+  }
+}
+
 void main()
 {
-  
-  
+  sem_init(&tobacco,0,0);
+  sem_init(&matches,0,0);
+  sem_init(&lock,0,1);
+  sem_init(&paper,0,0);
+  sem_init(&pusher_t,0,0);
+  sem_init(&pusher_m,0,0);
+  sem_init(&pusher_p,0,0);
+  sem_init(&agent,0,1);
+
+  pthread_t tid[9];
+  pthread_create(&tid[0],NULL,smokerT,NULL);
+  pthread_create(&tid[1],NULL,smokerP,NULL);
+  pthread_create(&tid[2],NULL,smokerM,NULL);
+  pthread_create(&tid[3],NULL,pusherT,NULL);
+  pthread_create(&tid[4],NULL,pusherP,NULL);
+  pthread_create(&tid[5],NULL,pusherM,NULL);
+  pthread_create(&tid[6],NULL,agentM,NULL);
+  pthread_create(&tid[7],NULL,agentP,NULL);
+  pthread_create(&tid[8],NULL,agentT,NULL);
+
+  sleep(10);
 }
